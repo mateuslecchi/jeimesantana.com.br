abee23d79041ebc34fcc331ea42358f621ec3e69
Mateus Lecchi <mateus.lecchi.y@gmail.com>
2020-01-13 17:40:37 -0300

    Set form action

--- form.php
+++ form.php
@@ -1,14 +1,11 @@
 <?php
-/**
- * This example shows making an SMTP connection with authentication.
- */
-//Import the PHPMailer class into the global namespace
+require 'assets/phpmailer/src/Exception.php';
+require 'assets/phpmailer/src/PHPMailer.php';
+require 'assets/phpmailer/src/SMTP.php';
+
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\SMTP;
 //SMTP needs accurate times, and the PHP time zone MUST be set
-//This should be done in your php.ini, but this is how to do it if you don't have access to that
-date_default_timezone_set('Etc/UTC');
-require '../vendor/autoload.php';
 //Create a new PHPMailer instance
 $mail = new PHPMailer;
 //Tell PHPMailer to use SMTP
@@ -17,35 +14,33 @@ $mail->isSMTP();
 // SMTP::DEBUG_OFF = off (for production use)
 // SMTP::DEBUG_CLIENT = client messages
 // SMTP::DEBUG_SERVER = client and server messages
-$mail->SMTPDebug = SMTP::DEBUG_SERVER;
+$mail->SMTPDebug = SMTP::DEBUG_OFF;
 //Set the hostname of the mail server
-$mail->Host = 'mail.example.com';
+$mail->Host = 'smtp.hostinger.com.br';
 //Set the SMTP port number - likely to be 25, 465 or 587
-$mail->Port = 25;
+$mail->Port = 587;
 //Whether to use SMTP authentication
 $mail->SMTPAuth = true;
 //Username to use for SMTP authentication
-$mail->Username = 'yourname@example.com';
+$mail->Username = 'contato@jeimesantana.com.br';
 //Password to use for SMTP authentication
-$mail->Password = 'yourpassword';
+$mail->Password = 'jeime123456';
 //Set who the message is to be sent from
-$mail->setFrom('from@example.com', 'First Last');
-//Set an alternative reply-to address
-$mail->addReplyTo('replyto@example.com', 'First Last');
+$mail->setFrom('contato@jeimesantana.com.br', $_POST['name']);
 //Set who the message is to be sent to
-$mail->addAddress('whoto@example.com', 'John Doe');
+$mail->addAddress('contato@jeimesantana.com.br', 'Jeime Santana');
 //Set the subject line
-$mail->Subject = 'PHPMailer SMTP test';
+$mail->Subject = 'Contato de '.$_POST['name'];
 //Read an HTML message body from an external file, convert referenced images to embedded,
 //convert HTML into a basic plain-text alternative body
-$mail->msgHTML(file_get_contents('contents.html'), __DIR__);
-//Replace the plain text body with one created manually
-$mail->AltBody = 'This is a plain-text message body';
-//Attach an image file
-$mail->addAttachment('images/phpmailer_mini.png');
-//send the message, check for errors
+$mail->msgHTML('
+    Nome: '.$_POST['name'].'<br>
+    Email: '.$_POST['email'].'<br>
+    Messagem: '.$_POST['message']
+);
 if (!$mail->send()) {
     echo 'Mailer Error: ' . $mail->ErrorInfo;
 } else {
     echo 'Message sent!';
+    header('Location: https://jeimesantana.com.br/');
 }
